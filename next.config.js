9041e623649e0949cce6acc9bdd0c7631933329b
Leo Cheung <leo.cheung@cbsinteractive.com>
2022-08-29 17:02:09 -0400

    add image domain exceptions

--- next.config.js
+++ next.config.js
@@ -2,6 +2,14 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  // add own custom domain names for image src
+  images: {
+    domains: [
+      "links.papareact.com",
+      "platform-lookaside.fbsbx.com",
+      "firebasestorage.googleapis.com"
+    ]
+  }
 }
 
 module.exports = nextConfig
