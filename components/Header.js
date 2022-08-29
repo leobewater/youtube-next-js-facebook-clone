d8be6366df07323894d714f03f15a520f59f55e2
Leo Cheung <leo.cheung@cbsinteractive.com>
2022-08-29 17:19:19 -0400

    add style to search box

--- components/Header.js
+++ components/Header.js
@@ -5,19 +5,18 @@ import { FlagIcon, PlayIcon, MagnifyingGlassIcon, ShoppingCartIcon } from '@hero
 function Header() {
   return (
     <div>
-      <h1>Header</h1>
-
       {/* Left */}
       <div className='flex items-center'>
         <Image src="https://links.papareact.com/5me" width={40} height={40} layout="fixed" alt="" />
 
-        <div className='flex'>
-          <MagnifyingGlassIcon className='h-6' />
-          <input type="text" placeholder='Search Facebook' />
+        <div className='flex ml-2 items-center rounded-full bg-gray-100 p-2'>
+          <MagnifyingGlassIcon className='h-6 text-gray-600' />
+          <input className='flex ml-2 items-center bg-transparent outline-none placeholder-gray-500' type="text" placeholder='Search Facebook' />
         </div>
       </div>
 
       {/* Center */}
+      
 
       {/* Right */}
 
