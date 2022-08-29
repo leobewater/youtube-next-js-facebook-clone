a2388fd8354af56b4f474a5af99d2285ab7a26cf
Leo Cheung <leo.cheung@cbsinteractive.com>
2022-08-29 19:04:27 -0400

    show login page if no user session

---
+++ components/Login.js
@@ -0,0 +1,11 @@
+import Image from 'next/image';
+
+function Login() {
+    return (
+        <div>
+            <Image src="https://links.papareact.com/t4i" height={400} width={400} objectFit="contain" alt="Logo" />
+        </div>
+    )
+}
+
+export default Login
--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,11 @@
 import Head from 'next/head'
 import Header from '../components/Header'
+import Login from '../components/Login'
+import { useSession, signIn, signOut, getSession } from "next-auth/react"
 
-export default function Home() {
+export default function Home({ session }) {
+  if (!session) return <Login />;
+
   return (
     <div>
       <Head>
@@ -21,4 +25,16 @@ export default function Home() {
       </main>
     </div>
   )
+}
+
+// server side rendering
+export async function getServerSideProps(context) {
+  // get the user
+  const session = await getSession(context);
+
+  return {
+    props: {
+      session
+    }
+  }
 }
