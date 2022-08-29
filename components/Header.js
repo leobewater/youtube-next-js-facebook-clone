92c65214f9c0d22f2a95a15e01ab6042049e2f89
Leo Cheung <leo.cheung@cbsinteractive.com>
2022-08-29 17:37:31 -0400

    populate header icons

--- components/Header.js
+++ components/Header.js
@@ -1,22 +1,31 @@
 import Image from 'next/image';
+import HeaderIcon from './HeaderIcon';
 import { BellIcon, ChatIcon, ChevronDownIcon, HomeIcon, UserGroupIcon, ViewGridIcon } from '@heroicons/react/24/solid';
 import { FlagIcon, PlayIcon, MagnifyingGlassIcon, ShoppingCartIcon } from '@heroicons/react/24/outline';
 
 function Header() {
   return (
-    <div>
+    <div className='sticky top-0 z-50 bg-white flex items-center p-2 lg:px-5 shadow-md'>
       {/* Left */}
       <div className='flex items-center'>
         <Image src="https://links.papareact.com/5me" width={40} height={40} layout="fixed" alt="" />
 
         <div className='flex ml-2 items-center rounded-full bg-gray-100 p-2'>
           <MagnifyingGlassIcon className='h-6 text-gray-600' />
-          <input className='flex ml-2 items-center bg-transparent outline-none placeholder-gray-500' type="text" placeholder='Search Facebook' />
+          <input className='flex ml-2 items-center bg-transparent outline-none placeholder-gray-500 flex-shrink' type="text" placeholder='Search Facebook' />
         </div>
       </div>
 
       {/* Center */}
-      
+      <div className='flex justify-center flex-grow'>
+        <div className='flex space-x-6'>
+          <HeaderIcon active Icon={HomeIcon} />
+          <HeaderIcon Icon={FlagIcon} />
+          <HeaderIcon Icon={PlayIcon} />
+          <HeaderIcon Icon={ShoppingCartIcon} />
+          <HeaderIcon Icon={UserGroupIcon} />
+        </div>
+      </div>
 
       {/* Right */}
 
