0d2347e45c8dc4e604745eec10e84ded00a73627
Leo Cheung <leo.cheung@cbsinteractive.com>
2022-08-31 17:36:18 -0400

    add widgets

--- pages/index.js
+++ pages/index.js
@@ -3,6 +3,7 @@ import Header from '../components/Header'
 import Login from '../components/Login'
 import Sidebar from '../components/Sidebar'
 import Feed from '../components/Feed'
+import Widgets from '../components/Widgets'
 import { getSession } from "next-auth/react"
 import { useSession } from "next-auth/react"
 
@@ -27,8 +28,8 @@ export default function Home() {
         <Sidebar />
 
         <Feed />
-        
-        {/* Widgets */}
+
+        <Widgets />
       </main>
     </div>
   )
